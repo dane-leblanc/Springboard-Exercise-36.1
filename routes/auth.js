262f5c73cd79343fad4a543ea04ae3cb82c8d637
Dane-LeBlanc <dane.leblanc@gmail.com>
2022-04-06 23:06:00 -0600

    all tests passing

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = new express.Router();
-const ExpressError = require("../expressError.js")
-const db = require("../db");
-const bcrypt = require("bcrypt");
+const ExpressError = require("../expressError.js");
 const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+const { SECRET_KEY } = require("../config.js");
 
 /** POST /login - login: {username, password} => {token}
  *
@@ -11,6 +11,23 @@ const jwt = require("jsonwebtoken");
  *
  **/
 
+router.post("/login", async function (req, res, next) {
+  try {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      throw new ExpressError("Username and password required", 400);
+    }
+    if (await User.authenticate(username, password)) {
+      let token = jwt.sign({ username }, SECRET_KEY);
+      User.updateLoginTimestamp(username);
+      return res.json({ token });
+    } else {
+      throw new ExpressError("Invalid username/password", 400);
+    }
+  } catch (err) {
+    return next(err);
+  }
+});
 
 /** POST /register - register user: registers, logs in, and returns token.
  *
@@ -18,3 +35,28 @@ const jwt = require("jsonwebtoken");
  *
  *  Make sure to update their last-login!
  */
+
+router.post("/register", async function (req, res, next) {
+  try {
+    const user = await User.register(req.body);
+    let username = user.username;
+    let token = jwt.sign({ username }, SECRET_KEY);
+    User.updateLoginTimestamp(user.username);
+    return res.json({ token });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+// router.post("/register", async function (req, res, next) {
+//   try {
+//     let { username } = await User.register(req.body);
+//     let token = jwt.sign({ username }, SECRET_KEY);
+//     User.updateLoginTimestamp(username);
+//     return res.json({ token });
+//   } catch (err) {
+//     return next(err);
+//   }
+// });
+
+module.exports = router;
--- routes/messages.js
+++ routes/messages.js
@@ -1,3 +1,15 @@
+const express = require("express");
+const router = new express.Router();
+const Message = require("../models/message");
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = require("../config");
+const {
+  authenticateJWT,
+  ensureLoggedIn,
+  ensureCorrectUser,
+} = require("../middleware/auth.js");
+const ExpressError = require("../expressError");
+
 /** GET /:id - get detail of message.
  *
  * => {message: {id,
@@ -11,6 +23,26 @@
  *
  **/
 
+router.get("/:id", ensureCorrectUser, async function (req, res, next) {
+  try {
+    const token = jwt.verify(req.body._token, SECRET_KEY);
+    const username = token.username;
+    let message = await Message.get(req.params.id);
+
+    if (
+      message.from_user.username !== username &&
+      message.to_user.username !== username
+    ) {
+      throw new ExpressError(
+        "You are not authorized to view this message",
+        400
+      );
+    }
+    return res.json({ message });
+  } catch (err) {
+    return next(err);
+  }
+});
 
 /** POST / - post message.
  *
@@ -19,6 +51,21 @@
  *
  **/
 
+router.post("/", ensureLoggedIn, async function (req, res, next) {
+  try {
+    let { from_username, to_username, body } = req.body;
+    if (!from_username || !to_username || !body) {
+      throw new ExpressError(
+        "You have not provided enough information to create a message",
+        400
+      );
+    }
+    let message = await Message.create();
+    return res.json(message);
+  } catch (err) {
+    return next(err);
+  }
+});
 
 /** POST/:id/read - mark message as read:
  *
@@ -28,3 +75,27 @@
  *
  **/
 
+router.post("/:id/read", ensureLoggedIn, async function (req, res, next) {
+  try {
+    const id = req.params.id;
+    const username = req.user.username;
+    const query = await db.query(
+      `SELECT * FROM messages
+             WHERE id=$1
+             AND to_username=$2`,
+      [id, username]
+    );
+    if (!query.rows.length) {
+      throw new ExpressError(
+        "You are not the intended recipient of this message",
+        400
+      );
+    }
+    const markAsRead = await Message.markRead(id);
+    return res.json(markAsRead);
+  } catch (err) {
+    return next(err);
+  }
+});
+
+module.exports = router;
